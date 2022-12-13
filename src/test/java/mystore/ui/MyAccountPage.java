00582e8dde4096b78bfc9f9c0d532e1f79cf53d2
john.arango <johnj42@gmail.com>
2022-12-13 13:51:45 -0500

    Xpath selector Changed to CssSelector

--- src/test/java/mystore/ui/MyAccountPage.java
+++ src/test/java/mystore/ui/MyAccountPage.java
@@ -6,6 +6,6 @@ import org.openqa.selenium.By;
 
 public class MyAccountPage {
     public static final Target LOGIN_STATUS = Target.the("My login status")
-            .located(By.xpath("//*[text()=' Logged in as ']"));
+            .located(By.cssSelector("li:nth-of-type(10) > a"));
 
 }
