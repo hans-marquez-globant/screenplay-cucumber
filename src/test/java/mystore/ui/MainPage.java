c791f716c08361b10c21cc18e2dbb5d04020a81e
am-garc1a <am.garcia@globant.com>
2022-12-12 22:46:15 -0500

    wait for element action added.

--- src/test/java/mystore/ui/MainPage.java
+++ src/test/java/mystore/ui/MainPage.java
@@ -4,9 +4,9 @@ import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
 
 public class MainPage {
-    public static final Target SECTION_LOGIN = Target.the("Login Section in main Page")
-            .located(By.cssSelector("a[href='/login']"));
+    public static final Target SECTION_LOGIN =
+            Target.the("Login Section in main Page").located(By.cssSelector("a[href='/login']"));
 
-    public static final Target SECTION_PRODUCTS = Target.the("Products Section in main Page")
-            .located(By.cssSelector("a[href='/products']"));
+    public static final Target SECTION_PRODUCTS =
+            Target.the("Products Section in main Page").located(By.cssSelector("a[href='/products']"));
 }
