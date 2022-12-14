287f285358a64e467884c778573e7d7f97211373
jordan.gonzalez <jordan.olaves91@gmail.com>
2022-12-14 13:29:49 -0500

    adding constants

--- src/test/java/mystore/constants/Constants.java
+++ src/test/java/mystore/constants/Constants.java
@@ -3,4 +3,6 @@ package mystore.constants;
 public class Constants {
     public static final String PAGINA_SIGN_IN = "https://automationexercise.com/";
     public static final String LOGIN_STATUS_TITLE = "Logged in as Test Automation";
+    public static final String LOGIN_INCORRECT_TEXT = "Your email or password is incorrect!";
+    public static final String LOGIN_TITLE = "Login to your account";
 }
