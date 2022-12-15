22dd57561d487e44c9bdafa60740100c99627ec5
Oscar Araque <oscar.araque@globant.com>
2022-12-15 11:42:06 -0500

    1st commit

--- src/test/java/mystore/actions/MyStoreHomePage.java
+++ src/test/java/mystore/actions/MyStoreHomePage.java
@@ -4,5 +4,5 @@ import mystore.constants.Constants;
 import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
 
-@DefaultUrl(Constants.PAGINA_SIGN_IN)
+@DefaultUrl(Constants.URL_SIGN_IN)
 public class MyStoreHomePage extends PageObject {}
---
+++ src/test/java/mystore/actions/WaitElements.java
@@ -0,0 +1,5 @@
+package mystore.actions;
+
+public class WaitElements {
+    
+}
