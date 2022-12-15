d82ede8ce1f45fd47622828e6cf91384a4992436
laura.cabezas <lauracs1213@gmail.com>
2022-12-15 10:06:36 -0500

    change selectors

--- src/test/java/mystore/ui/MyAccountPage.java
+++ src/test/java/mystore/ui/MyAccountPage.java
@@ -6,5 +6,5 @@ import org.openqa.selenium.By;
 
 public class MyAccountPage {
     public static final Target LOGIN_STATUS = Target.the("My login status")
-            .located(By.xpath("//*[text()=' Logged in as ']"));
+            .located(By.cssSelector("a:has(> i.fa-user)"));
 }
