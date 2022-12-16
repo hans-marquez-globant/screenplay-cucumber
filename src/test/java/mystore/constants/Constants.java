8fc0e6b2c4afdc67f88b7e8102daab80cdc6eb82
Juan Camilo Gonzalez <jcgonzalezb@gmail.com>
2022-12-15 20:27:23 -0500

    Constants updated!

--- src/test/java/mystore/constants/Constants.java
+++ src/test/java/mystore/constants/Constants.java
@@ -3,4 +3,7 @@ package mystore.constants;
 public class Constants {
     public static final String PAGINA_SIGN_IN = "https://automationexercise.com/";
     public static final String LOGIN_STATUS_TITLE = "Logged in as Test Automation";
+    public static final String LOGIN_TO_YOUR_ACCOUNT = "Login to your account";
+    public static final String WRONG_EMAIL_PASSWORD = "Your email or password is incorrect!";
+
 }
