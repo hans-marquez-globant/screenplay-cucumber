480a2bde74e2018d8ca28f2c2f1dc7e1e0e1f625
Zeus Vargas <zeusvargas26@gmail.com>
2022-12-16 01:55:45 -0500

    feat: added new css selectors

--- src/test/java/mystore/ui/MainPage.java
+++ src/test/java/mystore/ui/MainPage.java
@@ -7,5 +7,5 @@ public class MainPage {
     public static final Target SECTION_LOGIN = Target.the("Login Section in main Page")
             .located(By.cssSelector("a[href='/login']"));
     public static final Target SECTION_PRODUCTS = Target.the("Products Section in main Page")
-            .located(By.xpath("//*[@class='nav navbar-nav']//*[text()=' Products']"));
+            .located(By.cssSelector("a[href='/products']"));
 }
