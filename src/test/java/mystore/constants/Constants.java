ca5cb65a2e10615d4c890b1dce1f70a4b6e50efe
Xiomara Nataly Rodriguez Giraldo <nataly.rodriguez@globant.com>
2022-12-16 07:11:22 -0500

    Updating constants

--- src/test/java/mystore/constants/Constants.java
+++ src/test/java/mystore/constants/Constants.java
@@ -3,4 +3,7 @@ package mystore.constants;
 public class Constants {
     public static final String PAGINA_SIGN_IN = "https://automationexercise.com/";
     public static final String LOGIN_STATUS_TITLE = "Logged in as Test Automation";
+    public static final String LOGIN_WARNING_STATUS = "Your email or password is incorrect!";
+    public static final String LOGIN_TITLE = "Login to your account";
+
 }
